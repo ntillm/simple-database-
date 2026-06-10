20dae689ddbf4742caf98786cde174cac578e1f9
nate <natetillman3@gmail.com>
2026-06-10 16:07:49 -0700

    working on sql compiler and vm

--- main.c
+++ main.c
@@ -2,11 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+//type definitions 
 typedef struct{
   char* buffer;
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+typedef enum{
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+}StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement
 
 InputBuffer* new_input_buffer(){
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
@@ -36,20 +56,43 @@ void close_input_buffer(InputBuffer* input_buffer){
   free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+  if(strcmp(input_buffer->buffer, ".exit") == 0){
+    exit(EXIT_SUCCESS);
+  }else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+  if (strncmp(input_buffer->buffer, "insert",6) == 0){
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if(strcmp(input_buffer->buffer, "select") == 0){
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
 
 int main(int argc, char* argv[]){
   InputBuffer* input_buffer = new_input_buffer();
   while(1) {
     print_prompt();
     read_input(input_buffer);
-
-    if(strcmp(input_buffer->buffer, ".exit") == 0){
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-  }else{
-    printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+  } 
+  if(input_buffer->buffer[0] == '.') {
+    switch(do_meta_command(input_buffer)) {
+      case(META_COMMAND_SUCCESS):
+        continue;
+      case(META_COMMAND_UNRECOGNIZED):
+        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        continue;
     }
   }
+  
 }
 
 
